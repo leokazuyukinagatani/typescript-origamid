e8a16cb8eef193bfc09f6d1dd3e1e4fb35685371
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-11-15 11:48:14 -0300

    feat: aula0307

---
+++ aula0307/script.js
@@ -0,0 +1,15 @@
+"use strict";
+function typeGuard(value) {
+    if (typeof value === "string") {
+        return value.toUpperCase();
+    }
+    if (typeof value === "number") {
+        return value.toFixed();
+    }
+    if (value instanceof HTMLElement) {
+        return value.innerText;
+    }
+}
+console.log(typeGuard("Leo"));
+console.log(typeGuard(20.05));
+console.log(typeGuard(document.body));
