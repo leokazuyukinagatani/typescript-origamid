6f5b3492a3a1a535cb09b5cbe8861b74b43c077e
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-11-15 22:43:41 -0300

    feat: aula0311

---
+++ aula0311/script.js
@@ -0,0 +1,11 @@
+"use strict";
+function handleProdutoCarro(dados) {
+    dados.rodas;
+    dados.portas;
+    dados.preco;
+}
+handleProdutoCarro({ rodas: 4, portas: 5, preco: 30000 });
+function handleInterfaceCarro(carro) {
+    carro.portas;
+}
+// window.userId = 200
