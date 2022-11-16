05c2be3b9f0f414ba2295efa248ca401570d32cf
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-11-16 13:40:11 -0300

    feat: aula0403

--- aula0403/script.js
+++ aula0403/script.js
@@ -4,7 +4,84 @@
 //   preco: number
 //   novo: boolean
 // }
-function selecionar(seletor) {
-    return document.querySelector(seletor);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+// let chave: keyof Produto
+// let chave1: 'nome' | 'preco' | 'novo'
+// function coordenadas(x: number, y: number) {
+//   return { x, y }
+// }
+// let coord: typeof coordenadas
+// interface Elementos {
+//   a: HTMLAnchorElement
+//   video: HTMLVideoElement
+//   div: HTMLElement
+//   body: HTMLBodyElement
+//   audio: HTMLAudioElement
+// }
+// function selecionar<Chave extends keyof Elementos>(seletor:Chave): Elementos[Chave] | null {
+//   return document.querySelector(seletor)
+// }
+// selecionar('a')
+function fetchData(url) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const base = "https://api.origamid.dev/json";
+        const response = yield fetch(base + url);
+        return yield response.json();
+    });
 }
-selecionar('a');
+function isJogo(obj) {
+    if (obj && typeof obj === "object" && "desenvolvedora" in obj) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+/* verificando somente uma propriedade */
+// function checkInterface<T>(obj: unknown, key: keyof T): obj is T {
+//   if (obj && 
+//     typeof obj === "object" &&
+//     key in obj
+//     ) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+function checkInterface(obj, ...keys) {
+    if (obj &&
+        typeof obj === 'object' &&
+        keys.filter((key) => key in obj).length === keys.length) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+// async function handleData() {
+//   const jogo = await fetchData<Jogo>("/jogo.json");
+//   console.log(jogo);
+//   const livro: Livro = await fetchData<Livro>("/livro.json");
+//   console.log(livro);
+// }
+function handleData() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const jogo = yield fetchData("/jogo.json");
+        if (checkInterface(jogo, "desenvolvedora")) {
+            console.log(jogo.desenvolvedora);
+        }
+        const livro = yield fetchData("/livro.json");
+        if (checkInterface(livro, "autor")) {
+            console.log(livro.autor);
+        }
+    });
+}
+handleData();
--- aula0403/script.ts
+++ aula0403/script.ts
@@ -8,26 +8,106 @@
 
 // let chave1: 'nome' | 'preco' | 'novo'
 
-
 // function coordenadas(x: number, y: number) {
 //   return { x, y }
 // }
 
 // let coord: typeof coordenadas
 
-interface Elementos {
-  a: HTMLAnchorElement
-  video: HTMLVideoElement
-  div: HTMLElement
-  body: HTMLBodyElement
-  audio: HTMLAudioElement
+// interface Elementos {
+//   a: HTMLAnchorElement
+//   video: HTMLVideoElement
+//   div: HTMLElement
+//   body: HTMLBodyElement
+//   audio: HTMLAudioElement
+// }
+
+// function selecionar<Chave extends keyof Elementos>(seletor:Chave): Elementos[Chave] | null {
+//   return document.querySelector(seletor)
+// }
+
+// selecionar('a')
+
+async function fetchData<T>(url: string): Promise<T> {
+  const base = "https://api.origamid.dev/json";
+  const response = await fetch(base + url);
+  return await response.json();
+}
+
+interface Jogo {
+  nome: string;
+  ano: number;
+  desenvolvedora: string;
+  plataformas: string[];
+}
+
+interface Livro {
+  nome: string;
+  ano: number;
+  autor: string;
+  paginas: number;
+}
+
+function isJogo(obj: unknown): obj is Jogo {
+  if (obj && typeof obj === "object" && "desenvolvedora" in obj) {
+    return true;
+  } else {
+    return false;
+  }
+}
+/* verificando somente uma propriedade */
+// function checkInterface<T>(obj: unknown, key: keyof T): obj is T {
+//   if (obj && 
+//     typeof obj === "object" &&
+//     key in obj
+//     ) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+function checkInterface<Interface>(
+  obj: unknown,
+  ...keys: Array<keyof Interface>
+): obj is Interface {
+  if (
+    obj &&
+    typeof obj === 'object' &&
+    keys.filter((key) => key in obj).length === keys.length
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 
 
 
-function selecionar<Chave extends keyof Elementos>(seletor:Chave): Elementos[Chave] | null {
-  return document.querySelector(seletor)
+// async function handleData() {
+//   const jogo = await fetchData<Jogo>("/jogo.json");
+
+//   console.log(jogo);
+
+//   const livro: Livro = await fetchData<Livro>("/livro.json");
+
+//   console.log(livro);
+// }
+
+async function handleData() {
+  const jogo = await fetchData("/jogo.json");
+
+  if(checkInterface<Jogo>(jogo, "desenvolvedora")){
+    console.log(jogo.desenvolvedora);
+  }
+
+  const livro: Livro = await fetchData("/livro.json");
+
+  if(checkInterface<Livro>(livro, "autor")){
+    console.log(livro.autor);
+  }
+
 }
 
-selecionar('a')
+handleData();
